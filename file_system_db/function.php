caf56573dbb1448bb0d3f1ddff66662a6570743e
Zac <a19860101@gmail.com>
2020-09-05 15:32:43 +0800

    20200905

--- file_system_db/function.php
+++ file_system_db/function.php
@@ -5,3 +5,38 @@
         $stmt = $pdo->prepare($sql);
         $stmt->execute([$path,$title,$now]);
     }
+    function uploadImg($file){
+        $type = $file["type"];
+        $tmp_name = $file["tmp_name"];
+        $error = $file["error"];
+
+        switch($type){
+            case "image/jpeg":
+                $name = md5(time()).".jpg";
+            break;
+            case "image/png":
+                $name = md5(time()).".png";
+            break;
+            case "image/gif":
+                $name = md5(time()).".gif";
+            break;
+            default:
+                echo "檔案類型不正確";
+                return;
+        }
+        $target = "images/{$name}";
+
+        if($error === 0){
+            if(move_uploaded_file($tmp_name,$target)){
+                echo  "上傳成功";
+                header("Refresh:3;url=index.php");
+                return $name;
+            }else{
+                echo "上傳失敗";
+            }
+        }elseif($error === 4){
+            echo "請選擇檔案";
+        }else{
+            echo "上傳錯誤";
+        }
+    }
--- file_system_db/upload.php
+++ file_system_db/upload.php
@@ -1,35 +1,6 @@
 <?php
     include("function.php");
-    $type = $_FILES["img"]["type"];
-    $tmp_name = $_FILES["img"]["tmp_name"];
-    $error = $_FILES["img"]["error"];
-
-    switch($type){
-        case "image/jpeg":
-            $name = md5(time()).".jpg";
-        break;
-        case "image/png":
-            $name = md5(time()).".png";
-        break;
-        case "image/gif":
-            $name = md5(time()).".gif";
-        break;
-        default:
-            echo "檔案類型不正確";
-            return;
-    }
-    $target = "images/{$name}";
-
-    if($error === 0){
-        if(move_uploaded_file($tmp_name,$target)){
-            store($name,$_POST["title"]);
-            echo  "上傳成功";
-            header("Refresh:3;url=index.php");
-        }else{
-            echo "上傳失敗";
-        }
-    }elseif($error === 4){
-        echo "請選擇檔案";
-    }else{
-        echo "上傳錯誤";
-    }
+    
+    $file = $_FILES["img"];
+    $path = uploadImg($file);
+    store($path,$_POST["title"]);
