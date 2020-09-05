0c768155fbb5e6d40b49a2f72dd65ef2640d110b
Zac <a19860101@gmail.com>
2020-09-05 16:56:09 +0800

    20200905

---
+++ gd/img.php
@@ -0,0 +1,16 @@
+<?php
+    $img = "img.jpg";
+
+    $canvas = imagecreatefromjpeg($img);
+    $canvas_w = imagesx($canvas);
+    $canvas_h = imagesy($canvas);
+
+    $new_w = $canvas_w / 3;
+    $new_h = $canvas_h / 3;
+
+    $new_canvas = imagecreatetruecolor($new_w,$new_h);
+
+    imagecopyresampled($new_canvas,$canvas,0,0,0,0,$new_w,$new_h,$canvas_w,$canvas_h);
+    imagejpeg($new_canvas,"images/000.jpg");
+    imagepng($new_canvas,"images/000.png");
+    imagegif($new_canvas,"images/000.gif");
