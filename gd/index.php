d13caf762759ca26035752c791dd8f3df59a8a80
Zac <a19860101@gmail.com>
2020-09-05 16:44:14 +0800

    20200905

--- gd/index.php
+++ gd/index.php
@@ -11,7 +11,11 @@
     imagefill($canvas,0,0,$white);
     imageline($canvas,0,0,800,600,$red);
     imageline($canvas,0,600,800,0,$blue);
-
+    imagestring($canvas,1,50,50,"HELLO PHP",$black);
+    imagestring($canvas,2,50,70,"HELLO PHP",$red);
+    imagestring($canvas,3,50,90,"HELLO PHP",$blue);
+    imagestring($canvas,4,50,110,"HELLO PHP",$black);
+    imagestring($canvas,5,50,130,"HELLO PHP",$black);
     // header("content-type:image/jpeg");
     // imagejpeg($canvas);
     
@@ -19,4 +23,4 @@
     // imagegif($canvas);
 
     header("content-type:image/png");
-    imagepng($canvas);
+    imagepng($canvas,"images/".time().".png");
