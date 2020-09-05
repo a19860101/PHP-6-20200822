589a1e5ffa3a3e15a67959ed12c14cae1c2d4f78
Zac <a19860101@gmail.com>
2020-09-05 09:59:16 +0800

    20200905

--- pdo/index.php
+++ pdo/index.php
@@ -1,2 +1,51 @@
 <?php
-    require_once("pdo.php");
+    try {
+        require_once("pdo.php");
+        $sql = "SELECT * FROM students";
+        //預備陳述式
+        $stmt = $pdo->prepare($sql);
+        //執行sql
+        $stmt->execute();
+
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>學員資料表</title>
+</head>
+<body>
+    <h1>學員資料</h1>
+    <a href="create.php">新增學員資料</a>
+    <table border="1" width="600">
+        <tr>
+            <th>#</th>
+            <th>姓名</th>
+            <th>性別</th>
+            <th>電話</th>
+            <th>MAIL</th>
+            <th></th>
+        </tr>
+        <?php while($row = $stmt->fetch()){ ?>
+            <tr>
+                <td><?php echo $row["id"];?></td>
+                <td><?php echo $row["name"];?></td>
+                <td><?php echo $row["gender"];?></td>
+                <td><?php echo $row["phone"];?></td>
+                <td><?php echo $row["mail"];?></td>
+                <td>
+                    <form action="delete.php" method="post">
+                        <input type="hidden" name="id" value="<?php echo $row["id"];?>">
+                        <input type="submit" value="刪除" onclick="return confirm('確認刪除？')">
+                    </form>
+                    <a href="edit.php?id=<?php echo $row["id"]?>">編輯</a>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
+</body>
+</html>
