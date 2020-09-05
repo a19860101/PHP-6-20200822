a41ad2694db5574f441cc525256fdfe2bbec0f3e
Zac <a19860101@gmail.com>
2020-09-05 09:48:07 +0800

    20200905

--- pdo/pdo.php
+++ pdo/pdo.php
@@ -5,6 +5,12 @@
     $db_name = "sat";
     $db_charset = "utf8mb4";
 
-    $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
-    // $dsn = "mysql:host=localhost;dbname=sat;charset=utf8mb4";
-    $pdo = new PDO($dsn,$db_user,$db_pw);
+    //例外處理
+
+    try {
+        $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
+        // $dsn = "mysql:host=localhost;dbname=sat;charset=utf8mb4";
+        $pdo = new PDO($dsn,$db_user,$db_pw);
+    }catch(PDOException $e){
+        echo $e->getMessage();
+    }
