3c9b09b23a1ce7a2dfe934b5f5c007b2060843dc
Zac <a19860101@gmail.com>
2020-09-12 10:25:05 +0800

    20200912

--- session/index.php
+++ session/index.php
@@ -7,13 +7,29 @@
 </head>
 <body>
     <form action="store.php" method="post">
+        user
         <input type="text" name="user">
+        <br>
+        mail
+        <input type="text" name="mail">
+        <br>
+        phone
+        <input type="text" name="phone">
+        <br>
         <input type="submit" value="新增session">
     </form>
     <?php
         session_start();
         if($_SESSION){
-            echo $_SESSION["USER"];
+            // var_dump($_SESSION["PROFILE"]);
+
+            // foreach($_SESSION["PROFILE"] as $profile){
+                // echo $profile;
+            // }
+            echo $_SESSION["PROFILE"]["user"];
+            echo $_SESSION["PROFILE"]["mail"];
+            echo $_SESSION["PROFILE"]["phone"];
+
         }
     ?>
 </body>
--- session/store.php
+++ session/store.php
@@ -1,6 +1,9 @@
 <?php
     session_start();
 
-    $_SESSION["USER"] = $_POST["user"];
+    // $_SESSION["USER"] = $_POST["user"];
+
+    // var_dump($_POST);
+    $_SESSION["PROFILE"] = $_POST;
 
     header("location:index.php");
