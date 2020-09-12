0ecbf8b9701001fb42a7965619089e42462c8c7b
Zac <a19860101@gmail.com>
2020-09-12 09:55:33 +0800

    20200912

--- file_system_db/function.php
+++ file_system_db/function.php
@@ -42,6 +42,7 @@
             if(move_uploaded_file($tmp_name,$target)){
                 echo  "上傳成功";
                 header("Refresh:3;url=index.php");
+                imgResize($type,$target,$name);
                 return $name;
             }else{
                 echo "上傳失敗";
@@ -51,6 +52,31 @@
         }else{
             echo "上傳錯誤";
         }
+    }
+    function imgResize($type, $target, $name){
+        switch($type){
+            case "image/jpeg":
+                $canvas = imagecreatefromjpeg($target);
+            break;
+            case "image/png":
+                $canvas = imagecreatefrompng($target);
+            break;
+            case "image/gif":
+                $canvas = imagecreatefromgif($target);
+            break;
+        }
+        $canvas_w = imagesx($canvas);
+        $canvas_h = imagesy($canvas);
+
+        $new_w = 600;
+        $new_h = $canvas_h / $canvas_w * $new_w;
+
+        $new_canvas = imagecreatetruecolor($new_w,$new_h);
+        imagecopyresampled($new_canvas,$canvas,0,0,0,0,$new_w,$new_h,$canvas_w,$canvas_h);
+        imagejpeg($new_canvas,"thumbs/".$name);
+        imagepng($new_canvas,"thumbs/".$name);
+        imagegif($new_canvas,"thumbs/".$name);
+
     }
     function delete($id,$path){
         require_once("pdo.php");
--- gd/img.php
+++ gd/img.php
@@ -5,8 +5,11 @@
     $canvas_w = imagesx($canvas);
     $canvas_h = imagesy($canvas);
 
-    $new_w = $canvas_w / 3;
-    $new_h = $canvas_h / 3;
+    // $new_w = $canvas_w / 3;
+    // $new_h = $canvas_h / 3;
+
+    $new_w = 1000;
+    $new_h = $canvas_h / $canvas_w * $new_w;
 
     $new_canvas = imagecreatetruecolor($new_w,$new_h);
 
