7a372431fdca780b3beef6a6f91ae80a521561ee
Zac <a19860101@gmail.com>
2020-09-12 11:32:13 +0800

    20200912

---
+++ blog/function.php
@@ -0,0 +1,58 @@
+<?php
+    function showAll(){
+        try {
+            require_once("pdo.php");
+            $sql = "SELECT * FROM posts";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+            $rows = array();
+            while($row = $stmt->fetch()){
+                $rows[] = $row;
+            }
+            return $rows;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+    function show($id){
+        try {
+            require_once("pdo.php");
+            $sql = "SELECT * FROM students WHERE id = ?";
+            $stmt = $pdo->prepare($sql);
+            $stmt ->execute([$id]);
+            $row = $stmt->fetch();
+            return $row;
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+    function store($name,$phone,$mail,$gender){
+        try {
+            require_once("pdo.php");
+            $sql = "INSERT INTO students(name,phone,mail,gender,create_at)VALUES(?,?,?,?,?)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$name,$phone,$mail,$gender,$now]);
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+    function delete($id){
+        try{
+            require_once("pdo.php");
+            $sql = "DELETE FROM students WHERE id = ?";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$id]);
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
+    function update($name,$phone,$mail,$gender,$id){ 
+        try {
+            require_once("pdo.php");
+            $sql = "UPDATE students SET name=?,phone=?,mail=?,gender=? WHERE id = ?";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$name,$phone,$mail,$gender,$id]);
+        }catch(PDOException $e){
+            echo $e->getMessage();
+        }
+    }
--- blog/index.php
+++ blog/index.php
@@ -1,3 +1,23 @@
+<?php
+    include("function.php");
+    $rows = showAll();
+?>
 <?php include("template/header.php"); ?>
 <?php include("template/nav.php"); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <?php foreach($rows as $row){ ?>
+        <div class="col-8">
+            <h2><?php echo $row["title"];?></h2>
+            <div class="content">
+                <?php echo $row["content"]; ?>
+            </div>
+            <div>
+                建立時間 <?php echo $row["create_at"]; ?>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
+
+</div>
 <?php include("template/footer.php"); ?>
