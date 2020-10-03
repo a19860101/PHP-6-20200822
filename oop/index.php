38e69b8c2c53fd91ec25c4e2383c3acb7cc70e3c
Zac <a19860101@gmail.com>
2020-10-03 10:23:54 +0800

    20201003

--- oop/index.php
+++ oop/index.php
@@ -1,25 +1,28 @@
 <?php
     class Car {
-        public $doors = 4;
         public $color = 'black';
+        private $doors = 4;
+        //只能在類別內使用，不包括繼承類別
+        protected $wheels = 8;
+        //只能在類別內使用，包括繼承的類別
         Public function go(){
-            return 'gogo';
+            return $this->doors;
         }
     }
 
     class Truck extends Car {
-        public $weight = 1000;
+        public function test(){
+            return $this->wheels;
+        }
     }
 
+    $bmw = new Car;
+    // echo $bmw->doors;
+    echo $bmw->wheels;
+    // echo $bmw->go();
     $nissan = new Truck;
     // echo $nissan->doors;
     // echo $nissan->color = 'blue';
     // echo $nissan->go();
+    // echo $nissan->test();
 
-    class Train extends Car {
-
-    }
-
-    $test = new Train;
-    // echo $test->go();
-    echo $test->weight;
