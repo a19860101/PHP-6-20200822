d193b9ec751687cc74a4ce6c1a0ae783f25d5e01
Zac <a19860101@gmail.com>
2020-10-03 09:53:55 +0800

    20201003

--- oop/index.php
+++ oop/index.php
@@ -3,11 +3,24 @@
         //屬性(變數)
         public $x = 100;
         public $y = 'Hello';
+        
         //方法(函式)
+        public function go(){
+            return 'gogo!';
+        }
+
+        public function qq(){
+            $ans = $this->x * 2;
+            return $ans;
+        }   
+
     }
     
     //建立實體
     $test = new Test;
     // var_dump($test);
-    echo $test->x;
-    echo $test->y;
+    // echo $test->x;
+    // echo $test->y;
+
+    // echo $test->go();
+    echo $test->qq();
