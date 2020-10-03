f4cffaa449be1a8670abd0727846f8e74890c5e1
Zac <a19860101@gmail.com>
2020-10-03 16:25:06 +0800

    20201003

--- cms/app/Http/Controllers/PostController.php
+++ cms/app/Http/Controllers/PostController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     //
     function index(){
-        return view('post.index');
+        // return view('post.index');
+        $posts = DB::select('SELECT * FROM posts');
+        return view('post.index',compact('posts'));
     }
     function create(){
         return view('post.create');
--- cms/resources/views/post/index.blade.php
+++ cms/resources/views/post/index.blade.php
@@ -7,5 +7,15 @@
 </head>
 <body>
     <h1>post</h1>
+
+    @foreach($posts as $post)
+    <h2>{{$post->title}}</h2>
+    <div>
+        {{$post->content}}
+    </div>
+    <div>
+        建立時間:{{$post->created_at}}
+    </div>
+    @endforeach
 </body>
 </html>
