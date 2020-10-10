a67957a4334f0b2f1d61fa85a5698baa16f1e9e4
Zac <a19860101@gmail.com>
2020-10-10 15:00:00 +0800

    20201010

--- cms/app/Http/Controllers/PostController.php
+++ cms/app/Http/Controllers/PostController.php
@@ -97,5 +97,7 @@ class PostController extends Controller
     public function destroy($id)
     {
         //
+        DB::table('posts')->where('id',$id)->delete();
+        return redirect()->route('post.index');
     }
 }
