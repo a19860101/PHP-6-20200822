fce1241dde57cbcdefbde34961bb8d0eb16d36cd
Zac <a19860101@gmail.com>
2020-10-17 12:21:32 +0800

    20201017

--- cms/resources/views/post/create.blade.php
+++ cms/resources/views/post/create.blade.php
@@ -12,11 +12,11 @@
                 @csrf
                 <div class="form-group">
                     <label for="title">文章標題</label>
-                    <input type="text" name="title" id="title" class="form-control">
+                    <input type="text" name="title" id="title" class="form-control" value="{{old('title')}}">
                 </div>
                 <div class="form-group">
                     <label for="content">文章內容</label>
-                    <textarea name="content" id="content" rows="10" class="form-control"></textarea>
+                    <textarea name="content" id="content" rows="10" class="form-control">{{old('content')}}</textarea>
                 </div>
                 <input type="submit" class="btn btn-primary" value="新增文章">
                 <input type="button" class="btn btn-danger" value="取消" onclick="history.back()">
@@ -26,7 +26,9 @@
 </div>
 @if($errors->any())
     @foreach($errors->all() as $error)
-        <div>{{$error}}</div>
+    <div class="alert alert-danger mx-5" role="alert">
+        {{$error}}
+    </div>
     @endforeach
 @endif
 <script src="https://cdn.ckeditor.com/4.15.0/full/ckeditor.js"></script>
